1d231c05cb1164625ec3ae7f08f9b538c0a33d6f
Mani Ramezanpour <maniramznpoor2007@gmail.com>
2023-01-13 18:39:06 +0330

    use api example and change the coudintion

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 // http module
 const fs = require("fs");
 const http = require("http");
+const { users, post, album } = require("./Api");
 http
-  .createServer((req, response) => {
-    if (req.url == "/") {
-      const home = fs.readFileSync("./index.html");
-      response.write(home);
+  .createServer((req, res) => {
+    switch (req.url) {
+      case "/profile":
+        res.write(JSON.stringify(users));
+      case "/post":
+        res.write(JSON.stringify(post));
+      case "/album":
+        res.write(JSON.stringify(album));
+      default:
+        break;
     }
-    if (req.url == "/products") {
-      const products = fs.readFileSync("./product.html");
-      response.write(products);
-    }
-    response.end();
+    res.end();
   })
   .listen(2500, () => {
     console.log("http://localhost:2500");
