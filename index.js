49a855a50a84ae870183d69dae7529ce44155b04
Mani Ramezanpour <maniramznpoor2007@gmail.com>
2023-01-13 16:35:48 +0330

    make a server and work with http module

--- index.js
+++ index.js
@@ -1,40 +1,18 @@
-//* fs ==> file system == process with cpu == not good for cpu base project or file manager proejct
+// http module
 const fs = require("fs");
-
-const url = "test/admin/page";
-
-//? create directory
-// fs.mkdir(url, { recursive: true }, (error) => {
-//   console.log(error);
-// });
-
-//? write file
-// fs.writeFile(
-//   "fs-writeText.txt",
-//   "HI Mani its your first write file",
-//   "utf-8",
-//   (err) => console.log(err)
-// );
-
-//? append  data to file
-// fs.appendFile(
-//   "fs-writeText.txt",
-//   "\nits your second write file",
-//   "utf-8",
-//   (err) => console.log(err)
-// );
-
-//* sync : whait for result and after go to next line :blocking
-//*  nosync : dont wait for resule :nonblocking
-
-//? read directory data list
-// const files = fs.readdirSync("./");
-// for (const file of files) {
-//   console.log(file);
-// }
-
-//? delete file
-// fs.unlinkSync("./fs-writeText.txt", (err) => console.log(err));
-
-//? search to file exist or no 
-console.log(fs.existsSync("./uers.js"));
+const http = require("http");
+http
+  .createServer((req, response) => {
+    if (req.url == "/") {
+      const home = fs.readFileSync("./index.html");
+      response.write(home);
+    }
+    if (req.url == "/products") {
+      const products = fs.readFileSync("./product.html");
+      response.write(products);
+    }
+    response.end();
+  })
+  .listen(2500, () => {
+    console.log("http://localhost:2500");
+  });
