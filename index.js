14ea90f14ca715b00f5525db0405ee1ffd682081
Mani Ramezanpour <maniramznpoor2007@gmail.com>
2023-01-13 14:45:56 +0330

    work with fs method

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-const path = require('path');
+//* fs ==> file system == process with cpu == not good for cpu base project or file manager proejct
+const fs = require("fs");
 
-console.log(__dirname); // name of the directory
+const url = "test/admin/page";
 
-console.log(path.basename(__dirname)); // name of the file
+//? create directory
+// fs.mkdir(url, { recursive: true }, (error) => {
+//   console.log(error);
+// });
 
-console.log(path.dirname(__filename)); // the end pramter in adress or the folder dir
+//? write file
+// fs.writeFile(
+//   "fs-writeText.txt",
+//   "HI Mani its your first write file",
+//   "utf-8",
+//   (err) => console.log(err)
+// );
 
-console.log(path.extname(__filename)); // the format of file 
+//? append  data to file
+// fs.appendFile(
+//   "fs-writeText.txt",
+//   "\nits your second write file",
+//   "utf-8",
+//   (err) => console.log(err)
+// );
 
-console.log(path.parse(path.dirname(__filename))); // parse the file address
+//* sync : whait for result and after go to next line :blocking
+//*  nosync : dont wait for resule :nonblocking
+
+//? read directory data list
+// const files = fs.readdirSync("./");
+// for (const file of files) {
+//   console.log(file);
+// }
+
+//? delete file
+// fs.unlinkSync("./fs-writeText.txt", (err) => console.log(err));
+
+//? search to file exist or no 
+console.log(fs.existsSync("./uers.js"));
